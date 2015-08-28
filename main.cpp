1b369d029b51efa6bfb7ead75f85e238c1c66ee3
thang19xx <thang19xx@hotmail.com>
2015-08-28 13:49:44 +0700

    the first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "PrGlib.h"
+#include "RSA.h"
+#include "AES-CBC.h"
+
+using namespace std;
+#define _TIME int starts,finishs;
+#define STARTS_TIME starts=clock();
+#define FINISHS_TIME finishs=clock(); cout<<(double)(finishs-starts)/CLOCKS_PER_SEC<<endl;
+int main()
+{
+    //generate_RSA_Key("thang.pem");
+    //gen_public_RSA_Key("thang.pem","thang1.pem");
+    cryptRSA("text.txt","thang1.pem",0,"cry.txt");
+    decryptRSA("cry.txt","thang.pem",1,"decry.txt");
+}
